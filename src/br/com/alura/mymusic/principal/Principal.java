5d36bcd8d5dfdfc16a467acc35fd55db6d48e2ab
Alec Ikawa <72089401+alecikawa@users.noreply.github.com>
2025-04-28 13:23:18 -0300

    att

--- src/br/com/alura/mymusic/principal/Principal.java
+++ src/br/com/alura/mymusic/principal/Principal.java
@@ -1,4 +1,36 @@
 package br.com.alura.mymusic.principal;
 
+import br.com.alura.mymusic.modelos.MinhasPreferidas;
+import br.com.alura.mymusic.modelos.Musica;
+import br.com.alura.mymusic.modelos.Podcast;
+
 public class Principal {
+    public static void main(String[] args) {
+        Musica minhaMusica = new Musica();
+        minhaMusica.setTitulo("A bar Song");
+        minhaMusica.setCantor("Shaboozey");
+
+        for (int i = 0; i < 1000; i++) {
+            minhaMusica.reproduz();
+        }
+        for (int i = 0; i < 50; i++) {
+            minhaMusica.curte();
+        }
+
+        Podcast meuPodcast = new Podcast();
+        meuPodcast.setTitulo("DEVastador");
+        meuPodcast.setHost("Alec Ikawa");
+
+        for (int i = 0; i < 5000; i++) {
+            meuPodcast.reproduz();
+        }
+
+        for (int i = 0; i < 1000; i++) {
+            meuPodcast.curte();
+        }
+
+        MinhasPreferidas preferidas = new MinhasPreferidas();
+        preferidas.inclui(meuPodcast);
+        preferidas.inclui(minhaMusica);
+    }
 }
